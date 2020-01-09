de0f1cb3f1d556f63ff93aead9ee79737e07a635
Altaf Quadri <altafmquadri@gmail.com>
2020-01-08 20:10:51 -0500

    multiple pointers

--- index.js
+++ index.js
@@ -16,14 +16,12 @@
 //   function addUpTo(n) {
 //     return n * (n + 1) / 2;
 //   }
-  
 //   var time1 = performance.now();
 //   addUpTo(1000000000);
 //   var time2 = performance.now();
 //   console.log(`I am the faster one, Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
 
 //anagrams 
-// cinema and iceman
 //first count the number of chars in the word
 validAnagram = (word1, word2) => {
   if (word1.length !== word2.length) return false
@@ -52,3 +50,61 @@ validAnagram = (word1, word2) => {
   return true
 }
 
+
+//Multiple pointers pattern O(n^2) needs to be sorted array
+const sumZero = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]]
+      }
+    }
+  }
+}
+
+// console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]))
+
+//refactored SumZero
+const refactoredSumZero = (arr) => {
+  let left = 0
+  let right = arr.length-1
+  while (left < right) {
+    let sum = arr[left] + arr[right]
+    if (sum === 0) {
+      return [arr[left], arr[right]]
+    } else if (sum > 0) {
+      right--
+    } else {
+      left++
+    }
+  }
+}
+
+// console.log(refactoredSumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10]))
+// console.log(refactoredSumZero([-4, -3, -2, -1, 0, 5, 10]))
+
+const countUniqueValues = (arr) => {
+  //get a count of all values
+  let values = 0
+  let left = 0
+  let right = left + 1
+  while (right < arr.length) {
+    if (arr[left] === arr[right]) {
+      right++
+      left++
+    } else if (arr[left] !== arr[right]) {
+      values++
+      right++
+      left++
+    }
+  }
+  if (arr[arr.length -1] !== arr[arr.length - 2]) {
+    values++
+  } 
+  return values
+}
+
+console.log(countUniqueValues([1,1,1,1,1,2]))
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]))
+console.log(countUniqueValues([-2,-1,-1,0,1]))
+console.log(countUniqueValues([]))
