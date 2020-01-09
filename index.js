f77598ad9592b605b5a880dd4f11c7950c1ce7cd
Altaf Quadri <altafmquadri@gmail.com>
2020-01-09 18:50:04 -0500

    sliding window technique

--- index.js
+++ index.js
@@ -104,7 +104,67 @@ const countUniqueValues = (arr) => {
   return values
 }
 
-console.log(countUniqueValues([1,1,1,1,1,2]))
-console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]))
-console.log(countUniqueValues([-2,-1,-1,0,1]))
-console.log(countUniqueValues([]))
+// console.log(countUniqueValues([1,1,1,1,1,2]))
+// console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]))
+// console.log(countUniqueValues([-2,-1,-1,0,1]))
+// console.log(countUniqueValues([]))
+
+// **********************************************************************************************
+
+/* sliding window technique
+pattern involves creating a window which can either be an array or number from one
+position to another, depending on a certain condition, window either increases or closes
+and a new window is created, useful for keeping track of a subset of data in an array/string */
+
+//naive solution
+
+const maxSubarraySum = (arr, num) => {
+  if (num > arr.length) return null
+
+  let max = -Infinity
+
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    temp = 0
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j]
+    }
+    if (temp > max) {
+      max = temp
+    }
+    console.log(temp, max)
+  }
+  return max
+}
+
+// console.log(maxSubarraySum([2,6,9,2,1,8,5,6,3], 3))
+// console.log(maxSubarraySum([1,2,5,2,8,1,5], 2))
+// console.log(maxSubarraySum([1,2,5,2,8,1,5], 4))
+// console.log(maxSubarraySum([4,2,1,6], 1))
+// console.log(maxSubarraySum([4,2,1,6,2], 4))
+// console.log(maxSubarraySum([], 4))
+
+//refactored
+
+const refactoredMaxSubarraySum = (arr, num) => {
+  let maxSum = 0
+  let tempSum = 0
+  if (arr.length < num) return null
+  
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i]
+  }
+  tempSum = maxSum
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i]
+    maxSum = Math.max(maxSum, tempSum)
+  }
+  return maxSum
+}
+
+// console.log(refactoredMaxSubarraySum([2,6,9,2,1,8,5,6,3], 3))
+// console.log(refactoredMaxSubarraySum([1,2,5,2,8,1,5], 2))
+// console.log(refactoredMaxSubarraySum([1,2,5,2,8,1,5], 4))
+// console.log(refactoredMaxSubarraySum([4,2,1,6], 1))
+// console.log(refactoredMaxSubarraySum([4,2,1,6,2], 4))
+// console.log(refactoredMaxSubarraySum([], 4))
+
