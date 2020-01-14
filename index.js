feaf8dbe0c6c8d2da6bd6a0a84830ea2244f698f
Altaf Quadri <altafmquadri@gmail.com>
2020-01-14 00:01:20 -0500

    recursion bonus challenge p2

--- index.js
+++ index.js
@@ -649,10 +649,136 @@ const flatten = (arr) => {
 }
 
 
-console.log(flatten([1,2,[3]]))
+// console.log(flatten([1,2,[3]]))
 
-console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
-console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
-console.log(flatten([[1],[2],[3]])) // [1,2,3]
-console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
+// console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
+// console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+// console.log(flatten([[1],[2],[3]])) // [1,2,3]
+// console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
+
+/* Write a recusive fxn called CapitalizeFirst. Given an array of strings,
+capitalize the first letter of each string in the array */
+
+const capitalizeFirst = (words) => {
+  if (words.length === 1) return [words[0][0].toUpperCase() + words[0].slice(1)]
+  return [(words[0][0].toUpperCase() + words[0].slice(1)),  ...capitalizeFirst(words.slice(1))]
+}
+
+// console.log(capitalizeFirst(['car','taco','banana'])) // ['Car','Taco','Banana']
+
+/* Write a recursive function called nestedEvenSum. Return all even numbers
+in an object which may contain nested objects */
+
+const nestedEvenSum = (obj, sum=0) => {
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      sum += nestedEvenSum(obj[key])
+    } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0 ) {
+      sum += obj[key]
+    }
+  }
+  return sum
+}
+
+// var obj1 = {
+//   outer: 2,
+//   obj: {
+//     inner: 2,
+//     otherObj: {
+//       superInner: 2,
+//       notANumber: true,
+//       alsoNotANumber: "yup"
+//     }
+//   }
+// }
+
+// var obj2 = {
+//   a: 2,
+//   b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+//   c: {c: {c: 2}, cc: 'ball', ccc: 5},
+//   d: 1,
+//   e: {e: {e: 2}, ee: 'car'}
+// };
+
+// console.log(nestedEvenSum(obj1)) // 6
+// console.log(nestedEvenSum(obj2)) // 10
+
+/* Write a recursive fxn called capitalizeWords. Given an array of words
+return a new array containing each word capitalized */
+let words = ['i', 'am', 'learning', 'recursion']
+
+const capitalizeWords = (words) => {
+  if (words.length === 1) return [words[0].toUpperCase()]
+  return [words[0].toUpperCase(),  ...capitalizeWords(words.slice(1))]
+}
+
+// console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+/* Write a fxn called stringifyNumbers which takes in an object and finds all
+of the values which are numbers and converts them to strings */
+
+
+// let obj = {
+//     num: 1,
+//     test: [],
+//     data: {
+//         val: 4,
+//         info: {
+//             isRight: true,
+//             random: 66
+//         }
+//     }
+// }
+
+const stringifyNumbers = (obj) => {
+  const newObj = {}
+  for (let key in obj) {
+    if (typeof obj[key] === 'number') {
+      newObj[key] = obj[key].toString()
+    } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+        newObj[key] = stringifyNumbers(obj[key])
+      } else {
+          newObj[key] = obj[key]
+        }
+  }
+  return newObj
+}
+// console.log(stringifyNumbers(obj))
+
+/* Write a fxn called collectStrings which accepts an object and returns
+an array of all the values in the object that have a typeof string */
+
+
+const collectStrings = (obj) => {
+  let arr = []
+  for (let key in obj) {
+    if (typeof obj[key] === 'string') {
+      arr.push(obj[key])
+    } else if(typeof obj[key] === 'object') {
+        arr = [...arr, ...collectStrings(obj[key])]
+      }
+  }
+  return arr
+}
+
+// let obj = {
+//   stuff: "foo",
+//   data: {
+//       val: {
+//           thing: {
+//               info: "bar",
+//               moreInfo: {
+//                   evenMoreInfo: {
+//                       weMadeIt: "baz"
+//                   }
+//               }
+//           }
+//       }
+//   }
+// }
+
+
+
+// console.log(collectStrings(obj)) // ["foo", "bar", "baz"])
+
 
