f57fde4b7bc873c9c32eb3f791873e2afbd70052
Altaf Quadri <altafmquadri@gmail.com>
2020-01-27 14:16:20 -0500

    hash tables

--- index.js
+++ index.js
@@ -2185,12 +2185,12 @@ Priority Queue
 
 */
 
-class Node {
-  constructor(val, priority) {
-    this.val = val
-    this.priority = priority
-  }
-}
+// class Node {
+//   constructor(val, priority) {
+//     this.val = val
+//     this.priority = priority
+//   }
+// }
 
 class PriorityQueue {
   constructor(){
@@ -2266,3 +2266,93 @@ class PriorityQueue {
 // ER.enqueue("common cold", 5)
 // ER.enqueue("broken arm", 2)
 // ER.enqueue("high fever", 4)
+
+
+
+/*********************************************************************************************************** 
+                                                  Hash Tables
+set
+  1. Accepts a key and a value
+  2. Hashes the key
+  3. Stores the key-value pair in the hash table array via separate chaining
+
+get
+  1. Accepts a key
+  2. Hashes the key
+  3. Retrieves the key-value pair in the hash table
+  4. If the key isn't found, returns undefined
+
+keys
+  1. Loops through the hash table array and returns an array of keys in the table
+    values
+  2. Loops through the hash table array and returns an array of values in the table
+*/
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size)
+  }
+  _hash = (key) => {
+    let total = 0
+    let WEIRD_PRIME = 31
+    for (let i=0; i < Math.min(key.length, 100); i++) {
+      let char = key[i]
+      let value = char.charCodeAt(0) - 96
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length
+    }
+    return total
+  } //end _hash
+
+  set(key, value) {
+    let index = this._hash(key)
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = []
+    }
+    this.keyMap[index].push([key, value])
+  }
+
+  get(key) {
+    let index = this._hash(key)
+    if (this.keyMap[index]){
+      for (let i = 0; i < this.keyMap.length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1]
+        }
+      }
+    }
+    return undefined
+  }
+
+  keys() {
+    if (this.keyMap.length === 0) return
+    let keys = []
+    for (let i=0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j=0; j < this.keyMap[i].length; j++) {
+          if (!keys.includes(this.keyMap[i][j][0])) {
+            keys.push(this.keyMap[i][j][0])
+          }
+        }
+      }
+    }
+    return keys
+  }
+
+  values() {
+    if (this.keyMap.length === 0) return
+    let values = []
+    for (let i=0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j=0; j < this.keyMap[i].length; j++) {
+          if (!values.includes(this.keyMap[i][j][1])) {
+            values.push(this.keyMap[i][j][1])
+          }
+        }
+      }
+    }
+    return values
+  }
+
+}//end HashTable
+
+let ht = new HashTable(3)
